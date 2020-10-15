175deb4a56d3a87b7665b1b9757b72681945a27a
Hallan Pierre <hallanpierre@gmail.com>
2020-10-14 21:39:18 -0300

    Create service worker and register it on app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,21 @@
 class App {
     constructor() {
-        console.log('Construtor');
+        this.registerServiceWorker();
+    }
+
+    registerServiceWorker() {
+        if (!'serviceWorker' in navigator) {
+          console.info('Offline feature is not available on this browser');
+          return;
+        }
+    
+        const onsuccess = () => console.log('[Service Worker] Registered');
+        const onfailure = () => console.log('[Service Worker] Failed');
+    
+        navigator.serviceWorker
+          .register('sw.js')
+          .then(onsuccess)
+          .catch(onfailure)
     }
 }
   
